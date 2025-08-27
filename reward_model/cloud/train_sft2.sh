c5a6c88825a48fcea3646de8f5d5c4ca500435d8
ZejunCao <1378463428@qq.com>
2025-08-27 19:54:59 +0800

    test

---
+++ reward_model/cloud/train_sft2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export WANDB_BASE_URL=http://wandb.wml.weibo.com
+export WANDB_API_KEY=local-4cfba585394aeeb85fe732a406479591cd3344c6
+export WANDB_DIR=/sina-ml-runtime
+
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 /data0/anaconda3/envs/zejun7-latest/bin/deepspeed  train_sft2.py \
+    --model_name_or_path /data0/zejun7/vscode/zj_cloud/model_checkpoint/cloud_sft1_0825 \
+    --dataset_name /data0/zejun7/vscode/zj_cloud/data/sky_selfgen_dataset_0825.json \
+    --cutoff_len 2048 \
+    --output_dir save/cloud_sft2_0825 \
+    --learning_rate 1e-5 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 4 \
+    --deepspeed deepspeed/ds_z3_config.json \
+    --logging_steps 50 \
+    --save_steps 500 \
+    --eval_steps 500 \
+    --report_to wandb \
+    --run_name cloud_sft2_0825
